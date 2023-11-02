eb37b7ca8ba3e8dd9020f98915af25e38f817de9
maryclaireteahan <maryclaireteahan@gmail.com>
2023-11-02 14:32:07 +0000

    fix: correct spelling error in settings.py

--- letseat/settings.py
+++ letseat/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR = os.path.joim(BASE_DIR, 'templates')
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 if os.path.exists('env.py'):
     import env
