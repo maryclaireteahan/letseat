ae5f576a2e1574b99a88df20e17e9b86e998eb2f
maryclaireteahan <maryclaireteahan@gmail.com>
2023-11-06 23:55:44 +0000

    feat: Register Post model and PostAdmin class to admin site

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,12 @@
+from turtle import clear
 from django.contrib import admin
 from .models import Post
 from django_summernote.admin import SummernoteModelAdmin
 
+""" 
+Register Post model and PostAdmin class to admin site
+"""
+@admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-    summernote_fileds = ('content')
+    summernote_fields = ('content')
 
