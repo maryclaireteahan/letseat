f215dfbd3b06a4161877788ca1722f69ae13f6f6
maryclaireteahan <maryclaireteahan@gmail.com>
2023-11-07 10:43:31 +0000

    feat: add docstring

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from turtle import clear
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 """ 
@@ -8,5 +8,16 @@ Register Post model and PostAdmin class to admin site
 """
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
+    prepopulated_fields= {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
     summernote_fields = ('content')
+    list_display = ('title', 'slug', 'created_on')
+    search_fields = ['title', 'content']
+    
 
+""" 
+Register Comment model and CommentAdmin class to admin site
+"""
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    
