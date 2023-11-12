4234a7a257c943dc03e127c3b9043e508ba6ce11
maryclaireteahan <maryclaireteahan@gmail.com>
2023-11-12 19:48:46 +0000

    feat: connect urls with index

--- blog/views.py
+++ blog/views.py
@@ -7,4 +7,4 @@ class RecipeList(generic.ListView):
     queryset= Recipe.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6
-    
+
--- letseat/settings.py
+++ letseat/settings.py
@@ -143,4 +143,6 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
   
