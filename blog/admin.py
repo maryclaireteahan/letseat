0456e2ffcf33aa6d1b3e6ea00ed72c5a97bae193
maryclaireteahan <maryclaireteahan@gmail.com>
2023-11-13 11:39:20 +0000

    fix: debug = false

--- blog/admin.py
+++ blog/admin.py
@@ -11,7 +11,7 @@ class RecipeAdmin(SummernoteModelAdmin):
     prepopulated_fields= {'slug': ('title',)}
     list_filter = ('status', 'created_on')
     summernote_fields = ('content')
-    list_display = ('title', 'slug', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ['title', 'content']
     
 """ 
--- letseat/settings.py
+++ letseat/settings.py
@@ -27,9 +27,8 @@ if os.path.isfile('env.py'):
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
-
+DEBUG = False
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = ['maryclaireteahan-letseat.herokuapp.com', 'maryclaireteahan-letseat-0b5910f8e882.herokuapp.com', 'localhost', '8000', '127.0.0.1']
 
@@ -65,7 +64,7 @@ ROOT_URLCONF = 'letseat.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -135,8 +134,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
