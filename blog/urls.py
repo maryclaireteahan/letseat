efada42983f17d336d4ae7f46e317dac32cdd347
maryclaireteahan <maryclaireteahan@gmail.com>
2023-11-13 20:29:57 +0000

    fix: correct syntax error with path

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.RecipeHome.as_view(), name='home'),
-    path('/recipes', views.RecipeList.as_view(), name='recipes'),
-
+    path('recipes/', views.RecipeList.as_view(), name='recipes'),
 ]
