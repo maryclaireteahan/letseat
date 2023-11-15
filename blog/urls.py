86fa34e3c4bca129c676513301de16fface75daf
maryclaireteahan <maryclaireteahan@gmail.com>
2023-11-15 12:03:33 +0000

    feat: add allauth login logout signup

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.RecipeHome.as_view(), name='home'),
     path('recipes/', views.CategoryListView.as_view(), name='recipes'),
     path("<slug:slug>/", views.RecipeDetail.as_view(), name="single_recipe"),
-    path('recipe/<slug:slug>/like/', views.RecipeLike.as_view(), name='recipe_like'),
+    path('like/<slug:slug>', views.RecipeLike.as_view(), name='recipe_like'),
 ]
--- letseat/settings.py
+++ letseat/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
@@ -48,15 +52,23 @@ INSTALLED_APPS = [
     'blog',
 ]
 
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'allauth.account.middleware.AccountMiddleware',  # Place it here
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 
 ROOT_URLCONF = 'letseat.urls'
 
@@ -78,6 +90,7 @@ TEMPLATES = [
 
 
 WSGI_APPLICATION = 'letseat.wsgi.application'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
  
 # Database
