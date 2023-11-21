7e82c00ec342dbc648b4d29f28c310ecfb9d7d9d
maryclaireteahan <maryclaireteahan@gmail.com>
2023-11-21 12:30:44 +0000

    fix: remove comment approval views.py

--- blog/views.py
+++ blog/views.py
@@ -23,7 +23,7 @@ class RecipeDetail(View):
     def get(self, request, slug, *args, **kwargs):
         queryset= Recipe.objects.filter(status=1)
         recipe = get_object_or_404(queryset, slug=slug)
-        comments = recipe.comments.filter(approved=True).order_by('-created_on')
+        comments = recipe.comments.order_by('-created_on')
         liked = False
         context = {
             'recipe': recipe,
@@ -41,7 +41,7 @@ class RecipeDetail(View):
     def post(self, request, slug, *args, **kwargs):
         queryset= Recipe.objects.filter(status=1)
         recipe = get_object_or_404(queryset, slug=slug)
-        comments = recipe.comments.filter(approved=True).order_by('-created_on')
+        comments = recipe.comments.order_by('-created_on')
         liked = False
         if recipe.likes.filter(id=self.request.user.id).exists():
             liked = True
