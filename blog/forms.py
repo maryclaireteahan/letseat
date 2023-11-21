b77f35783080b5538df4cfecd068ab02d5f05305
maryclaireteahan <maryclaireteahan@gmail.com>
2023-11-21 10:18:06 +0000

    feat: add RecipeForm in forms.py frontend UI admin

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,16 @@
-from .models import Comment
+from .models import Comment, Recipe
 from django import forms
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model=Comment
         fields = ('body',)
+        
+class RecipeForm(forms.ModelForm):
+    class Meta:
+        model=Recipe
+        fields = ('title', 'author', 'ingredients', 'instructions', 'featured_image', 'image_alt', 'category')
+        
+
+
+
