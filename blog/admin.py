b61e29709145da3c9affb794e9fb5175f12d5fa5
maryclaireteahan <maryclaireteahan@gmail.com>
2023-11-26 23:32:13 +0000

    fix: reformat admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -2,33 +2,32 @@ from django.contrib import admin
 from .models import Recipe, Comment, Category
 from django_summernote.admin import SummernoteModelAdmin
 
+
 @admin.register(Recipe)
 class RecipeAdmin(SummernoteModelAdmin):
-    """ 
+    """
     Register Recipe model and RecipeAdmin class to admin site
     """
-    prepopulated_fields= {'slug': ('title',)}
-    list_filter = ( 'created_on', 'category')
-    summernote_fields = ('ingredients','instructions')
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('created_on', 'category')
+    summernote_fields = ('ingredients', 'instructions')
     list_display = ('title', 'slug', 'created_on')
     search_fields = ['title', 'content']
 
-    
+
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    """ 
+    """
     Register Comment model and CommentAdmin class to admin site
     """
     list_display = ('name', 'body', 'post', 'created_on')
-    list_filter = ( 'created_on', )
+    list_filter = ('created_on', )
     search_fields = ('name', 'email', 'body')
 
 
 @admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin): 
-    """ 
+class CategoryAdmin(admin.ModelAdmin):
+    """
     Register Category model and CategoryAdmin class to admin site
-    """  
-    prepopulated_fields= {'slug': ('name',)}
-
-
+    """
+    prepopulated_fields = {'slug': ('name',)}
