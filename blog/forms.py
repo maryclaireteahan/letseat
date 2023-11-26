d3f7c0771fd8a1978f49fc2b3269a1ff4e026c60
maryclaireteahan <maryclaireteahan@gmail.com>
2023-11-26 23:35:05 +0000

    fix: reformat forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -1,23 +1,27 @@
 from .models import Comment, Recipe
 from django import forms
 
+
 class CommentForm(forms.ModelForm):
     """
     Form for commenting on single recipes
     """
     class Meta:
-        model=Comment
+        model = Comment
         fields = ('body',)
-        
+
+
 class RecipeForm(forms.ModelForm):
     """
     Form for creating on single recipes on the frontend
     """
     class Meta:
-        model=Recipe
-        fields = ['title', 'author', 'ingredients', 'instructions', 'featured_image', 'image_alt', 'category', ]
-        
-
-
-
-
+        model = Recipe
+        fields = [
+            'title',
+            'author',
+            'ingredients',
+            'instructions',
+            'featured_image',
+            'image_alt',
+            'category', ]
